bc4f689c25983b0a05a9257a525c718dc744f752
AlexeiCheeseLover <AlexeiEmailLover@proton.me>
2026-03-25 02:00:22 +0530

    me testing

--- ass.py
+++ ass.py
@@ -5,7 +5,6 @@ try:
     with open("afile", 'w') as f:
         for i in range(24):
             ass = subprocess.run([f'''ping 192.168.1.{i} -c 1 | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" '''], capture_output=True, shell=True, text=True)
-            #print(ass.stdout)
             if (ass.stdout != b''):
                 print(ass.stdout, file=f)
 except KeyboardInterrupt:
